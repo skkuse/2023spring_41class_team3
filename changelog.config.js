708d8dfb1ce6abd1e59da4057d6d9a7756f6f1b5
AnJeongMin <ajhappy12@gmail.com>
2023-03-28 17:49:01 +0900

    fix: (common) - commitizen library 변경
    기존 cz-customizable은 format 변경이 너무 한정적이라서 git-cz를 사용

---
+++ changelog.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  disableEmoji: true,
+  format: "{type}: {scope} - {subject}",
+  list: ["feat", "fix", "refactor", "style", "chore", "test", "docs"],
+  maxMessageLength: 100,
+  minMessageLength: 1,
+  questions: ["type", "scope", "subject", "body"],
+  scopes: ["front", "back", "common"],
+  types: {
+    chore: {
+      description: "Build process or auxiliary tool changes",
+      value: "chore",
+    },
+    docs: {
+      description: "Documentation only changes",
+      value: "docs",
+    },
+    feat: {
+      description: "A new feature",
+      value: "feat",
+    },
+    fix: {
+      description: "A bug fix",
+      value: "fix",
+    },
+    refactor: {
+      description: "A code change that neither fixes a bug or adds a feature",
+      value: "refactor",
+    },
+    test: {
+      description: "Adding missing tests",
+      value: "test",
+    },
+    style: {
+      description: "Changes that do not affect the meaning of the code",
+      value: "style",
+    },
+  },
+};
