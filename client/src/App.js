07b7ac95f517a8ba3ee65c2caf8e01ab150da8f0
minsung1386 <denfor123@naver.com>
2023-05-02 01:49:02 +0900

    chore: (front) package.json 추가 및 불필요한 파일 삭제

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
-	);
-}
+import Login from './components/Login';
+import LandingPage from './pages/LandingPage';
+
+const App = () => {
+    const [user, setUser] = useState(null);
+
+    const handleLoginSuccess = (userInfo) => {
+        setUser(userInfo);
+    };
+
+    const handleLogout = () => {
+        setUser(null);
+    };
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route exact path="/" element={<LandingPage handleLoginSuccess={handleLoginSuccess} />} />
+                <Route path="/login" element={<Login handleLoginSuccess={handleLoginSuccess} />} />
+                <Route path="/main" element={<LandingPage user={user} handleLogout={handleLogout} />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
 
 export default App;
