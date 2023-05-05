1d1afe1be39351e78b65ce66fbae7abf83afc052
minsung1386 <denfor123@naver.com>
2023-05-06 00:51:30 +0900

    feat: (front) 로그인 컴포넌트 추가

---
+++ client/src/components/Landing/LoginContainer.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import LoginButton from "components/LoginButton";
+import LogoutButton from "components/LogoutButton";
+
+
+const LoginWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+`;
+
+
+function LoginContainer({user, handleLogout}) {
+    return (
+        <LoginWrapper>
+            { user
+            ? <LogoutButton handleLogout={handleLogout}/> 
+            : <LoginButton/> }
+        </LoginWrapper>
+    );
+}
+
+
+export default LoginContainer;
