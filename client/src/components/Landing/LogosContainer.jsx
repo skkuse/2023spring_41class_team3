188f8cd69e56a5512470be907af8e6e63a770236
minsung1386 <denfor123@naver.com>
2023-05-06 00:52:43 +0900

    feat: (front) 랜딩 페이지 로고 container 추가

---
+++ client/src/components/Landing/LogosContainer.jsx
@@ -0,0 +1,18 @@
+import LogoList from 'components/common/LogoList';
+import styled from 'styled-components';
+
+
+const LogosWrapper = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+`;
+
+function LogosContainer() {
+    return (
+        <LogosWrapper>
+            <LogoList/>
+        </LogosWrapper>
+    );
+}
+
+export default LogosContainer;
