e3381cfc7ea6d82393cf6abb0a6cf942ffaf983e
AnJeongMin <ajhappy12@gmail.com>
2023-05-19 22:32:40 +0900

    feat: (back) user 관련 boilerplate directory 생성

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,12 +6,14 @@ import { envConfig } from '@config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CatModule } from './cat/cat.module';
 import { mongooseConfig } from './config/mongoose.config';
+import { UserModule } from './user/user.module';
 
 @Module({
 	imports: [
 		ConfigModule.forRoot(envConfig),
 		MongooseModule.forRootAsync(mongooseConfig),
 		CatModule,
+		UserModule,
 	],
 	controllers: [AppController],
 	providers: [AppService],
