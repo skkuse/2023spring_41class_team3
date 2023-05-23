75f4538590e4e04044c76ca5523783eb4117c02f
AnJeongMin <ajhappy12@gmail.com>
2023-05-24 00:35:44 +0900

    feat: (back) mongoose exception filter 추가

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpLoggingInterceptor } from './interceptor/http.interceptor';
 import { HttpExceptionFilter } from './filter/http.filter';
+import { MongooseExceptionFilter } from './filter/mongoose.filter';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.useGlobalInterceptors(new HttpLoggingInterceptor());
 	app.useGlobalFilters(new HttpExceptionFilter());
+	app.useGlobalFilters(new MongooseExceptionFilter());
 	app.setGlobalPrefix('api');
 
 	await app.listen(8080);
