eac890b49bc5ac962c8a42fdc1dfc8d39236714e
AnJeongMin <ajhappy12@gmail.com>
2023-05-24 17:46:09 +0900

    feat: (back) JWT access token strategy 구현

---
+++ server/src/user/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,34 @@
+import { ACCESS_STRATEGY_KEY, JWT_ACCESS_TOKEN_SECRET } from '@constant';
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { AuthService } from '../service/auth.service';
+import { Request } from 'express';
+import { ConfigService } from '@nestjs/config';
+import { JwtPayload } from '@type';
+
+@Injectable()
+export class JwtAccessTokenSStrategy extends PassportStrategy(Strategy, ACCESS_STRATEGY_KEY) {
+	constructor(
+		private readonly authService: AuthService,
+		private readonly configService: ConfigService
+	) {
+		super({
+			ignoreExpiration: false,
+			jwtFromRequest: ExtractJwt.fromExtractors([
+				(req: Request) => {
+					const token = req?.cookies.accessToken;
+					return token ?? null;
+				},
+			]),
+			secretOrKey: configService.get(JWT_ACCESS_TOKEN_SECRET),
+			passReqToCallback: true,
+		});
+	}
+
+	async validate(req: Request, payload: JwtPayload) {
+		const accessToken = this.authService.createAccessToken(payload);
+		req.cookies.accessToken = accessToken;
+		return payload;
+	}
+}
