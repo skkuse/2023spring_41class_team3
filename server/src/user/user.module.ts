8f6e02e81d34fed2e81b66541a625e15df79fc5b
AnJeongMin <ajhappy12@gmail.com>
2023-05-24 23:19:21 +0900

    fix: (back) jwt 관련 의존성 주입 수정

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -6,10 +6,19 @@ import { ConfigService } from '@nestjs/config';
 import { UserRepository } from './auth/repository/auth.repository';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './entities/user.entity';
+import { JwtService } from '@nestjs/jwt';
+import { JwtAccessTokenStrategy } from './auth/strategy/jwt.strategy';
 
 @Module({
 	imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
 	controllers: [AuthController],
-	providers: [AuthService, GitHubOauth, ConfigService, UserRepository],
+	providers: [
+		AuthService,
+		GitHubOauth,
+		ConfigService,
+		UserRepository,
+		JwtService,
+		JwtAccessTokenStrategy,
+	],
 })
 export class UserModule {}
