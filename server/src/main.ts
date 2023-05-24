d4e2dadbc22a39c6f88d5559c2f6ffcb1e0c8e40
AnJeongMin <ajhappy12@gmail.com>
2023-05-24 17:37:39 +0900

    feat: (back) 쿠키 파서 등록

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,9 +3,11 @@ import { AppModule } from './app.module';
 import { HttpLoggingInterceptor } from './interceptor/http.interceptor';
 import { HttpExceptionFilter } from './filter/http.filter';
 import { MongooseExceptionFilter } from './filter/mongoose.filter';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	app.use(cookieParser());
 	app.useGlobalInterceptors(new HttpLoggingInterceptor());
 	app.useGlobalFilters(new HttpExceptionFilter());
 	app.useGlobalFilters(new MongooseExceptionFilter());
