4a01be7ed40b2fabb3297a20c1470550c3921f6d
AnJeongMin <ajhappy12@gmail.com>
2023-05-30 01:30:05 +0900

    feat: (front) Dropdown menu 로직 수정

--- client/src/components/Main/CodingTestModal.jsx
+++ client/src/components/Main/CodingTestModal.jsx
@@ -2,37 +2,39 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 
-import mainbgImage from 'assets/images/imagesource/MainPage_background.png';
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+
 import startCodingTest from 'assets/images/imagesource/MainPage_StartCodingTest.png';
 
 import FormControl from '@mui/material/FormControl';
-import NativeSelect from '@mui/material/NativeSelect';
-import { InputLabel } from '@mui/material';
 
 const Overlay = styled.div`
 	position: fixed;
 	width: 100%;
 	height: 100%;
 	top: 0;
-	bottom: 0;
 	left: 0;
-	right: 0;
+	background-color: rgba(0, 0, 0, 0.3);
 	z-index: 9999;
 `;
 
-const ModalWrap = styled.div`
-	width: 50%;
+const ModalWrap = styled.dialog`
+	width: 45%;
 	min-height: 45vh;
-	background-image: url(${mainbgImage});
+	background: #ffffff;
 	background-repeat: no-repeat;
 	background-size: cover;
 	background-position: center center;
-	border-radius: 60px;
+	border-radius: 30px;
+	border: 2px solid #d9d9d9;
 	position: absolute;
-	top: 58%;
+	top: 50%;
 	left: 50%;
 	transform: translate(-50%, -50%);
 	justify-content: center;
+	cursor: default;
+	display: block;
 `;
 
 const Contents = styled.div`
@@ -50,26 +52,25 @@ const Contents = styled.div`
 const Title = styled.div`
 	display: flex;
 	align-items: center;
-	margin: 0 auto;
+	margin: 0 auto 36px auto;
 	font-size: 25px;
 	font-weight: 600;
-	margin-bottom: 30px;
 	color: #3b3838;
 	text-align: center;
 `;
 
 const DropDownBody = styled.div`
 	width: 50%;
-	margin: 0 auto;
+	margin: 16px auto 0px auto;
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
+	gap: 32px;
 `;
 
 const UpdateTime = styled.div`
-	background-color: #d9d9d9;
 	margin: 3% 2%;
-	height: 3rem;
+	height: 2rem;
 	border-radius: 10px;
 	width: 96%;
 	font-size: 20px;
@@ -94,110 +95,76 @@ const StartCodingTestButton = styled.button`
 	display: inline-block;
 `;
 
-const DIFF_DATA = [
-	{ id: '0', diff: '난이도를 선택하세요' },
-	{ id: '1', diff: '1(매우 쉬움)' },
-	{ id: '2', diff: '2(조금 쉬움)' },
-	{ id: '3', diff: '3(중간)' },
-	{ id: '4', diff: '4(조금 어려움)' },
-	{ id: '5', diff: '5(매우 어려움)' },
-];
+const DropDownWrapper = styled.div`
+	width: 100%;
+	display: flex;
+	flex-direction: row;
+	-webkit-box-align: center;
+	align-items: center;
+	-webkit-box-pack: justify;
+	justify-content: space-evenly;
+`;
 
-const PROBNUM_DATA = [
-	{ id: '0', num: '문제 수를 선택하세요' },
-	{ id: '1', num: '1개' },
-	{ id: '2', num: '2개' },
-	{ id: '3', num: '3개' },
-	{ id: '4', num: '4개' },
-	{ id: '5', num: '5개' },
-];
+const problemDiffMenu = [1, 2, 3, 4, 5];
+const problemNumberMenu = [1, 2, 3, 4, 5];
 
-function CodingTestModal() {
-	const [diffselected, setDiffSelected] = useState('난이도를 선택하세요');
-	const [probnumselected, setProbNumSelected] = useState('문제 수를 선택하세요');
+function CodingTestModal({ close }) {
+	const [diffselected, setDiffSelected] = useState(1);
+	const [probnumselected, setProbNumSelected] = useState(1);
+	const modalRef = React.useRef();
 
 	const handleDropDownDiff = (e) => {
-		setDiffSelected(e.target.value);
+		setDiffSelected(e.value);
 	};
 	const handleDropDownProbNum = (e) => {
-		setProbNumSelected(e.target.value);
+		setProbNumSelected(e.value);
 	};
 
 	const navigate = useNavigate();
 
 	const handleStartCodingTest = () => {
-		navigate('/login');
-		// navigate('/test');
+		navigate('/test');
+	};
+
+	const handleOutsideClick = (e) => {
+		if (!modalRef.current.contains(e.target)) {
+			close();
+		}
 	};
 
 	return (
-		<Overlay>
-			<ModalWrap>
+		<Overlay onClick={handleOutsideClick}>
+			<ModalWrap ref={modalRef}>
 				<Contents>
-					<Title>
-						코딩 테스트 시뮬레이션
-						<br />
-						Setting
-					</Title>
+					<Title>코딩 테스트 시뮬레이션</Title>
 					<DropDownBody>
 						<FormControl>
-							<InputLabel>난이도</InputLabel>
-							<NativeSelect
-								onChange={handleDropDownDiff}
-								inputProps={{
-									name: 'diff',
-									id: 'uncontrolled-native',
-								}}
-								style={{
-									backgroundColor: '#FFFFFF',
-									margin: '3% 2%',
-									height: '3rem',
-									borderRadius: '10px',
-									width: '96%',
-									fontSize: '20px',
-									fontWeight: '400',
-									textAlignLast: 'center',
-								}}
-							>
-								{DIFF_DATA.map((el) => (
-									<option style={{ textAlign: 'center' }} key={el.id}>
-										{el.diff}
-									</option>
-								))}
-							</NativeSelect>
+							<DropDownWrapper>
+								<span>난이도</span>
+								<Dropdown
+									options={problemDiffMenu}
+									onChange={handleDropDownDiff}
+									value={`${diffselected} 단계`}
+									placeholder="문제 난이도를 선택하세요"
+								/>
+							</DropDownWrapper>
 						</FormControl>
 						<FormControl>
-							<InputLabel>문제 수</InputLabel>
-							<NativeSelect
-								onChange={handleDropDownProbNum}
-								inputProps={{
-									name: 'probnumber',
-									id: 'uncontrolled-native',
-								}}
-								style={{
-									alignContent: 'center',
-									backgroundColor: '#FFFFFF',
-									margin: '3% 2%',
-									height: '3rem',
-									borderRadius: '10px',
-									width: '96%',
-									fontSize: '20px',
-									fontWeight: '400',
-									textAlignLast: 'center',
-								}}
-							>
-								{PROBNUM_DATA.map((el) => (
-									<option style={{ textAlign: 'center' }} key={el.id}>
-										{el.num}
-									</option>
-								))}
-							</NativeSelect>
+							<DropDownWrapper>
+								<span>문제 수</span>
+								<Dropdown
+									options={problemNumberMenu}
+									onChange={handleDropDownProbNum}
+									value={`${probnumselected} 개`}
+									placeholder="문제 수를 선택하세요"
+								/>
+							</DropDownWrapper>
 						</FormControl>
 						<UpdateTime>
 							예상 소요시간은{' '}
-							{(DIFF_DATA.find((el) => el.diff === diffselected) || {}).id *
-								(PROBNUM_DATA.find((el) => el.num === probnumselected) || {}).id *
-								10}
+							<span style={{ color: '#0089ff' }}>
+								{diffselected * probnumselected * 10}
+							</span>
 							분입니다
 						</UpdateTime>
 						<StartCodingTestButton onClick={handleStartCodingTest} />
