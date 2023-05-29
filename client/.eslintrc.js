fc864029ead182ca929d4521264be223b3ba9b59
AnJeongMin <ajhappy12@gmail.com>
2023-05-30 01:30:47 +0900

    chore: (front) eslint 설정 수정
    package library를 제대로 읽지 못 하고 있음

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
 		'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
 		'react/react-in-jsx-scope': 'off',
 		'react/prop-types': 'off',
+		'import/no-extraneous-dependencies': 'off',
 		// react 함수형 컴포넌트 정의 방식 선언
 		'react/function-component-definition': [
 			2,
