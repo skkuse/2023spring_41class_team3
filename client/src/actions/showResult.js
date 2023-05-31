f3241ea8eaabdf95e9b83a5a91c8d2ce1d9ecca9
minsung1386 <denfor123@naver.com>
2023-05-31 17:36:22 +0900

    feat: (front) 결과 페이지 상태 관리

---
+++ client/src/actions/showResult.js
@@ -0,0 +1,52 @@
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+
+// 서버에서 fetch
+export function fetchResults() {
+	const dispatch = useDispatch();
+
+	axios
+		.get('http://localhost:3000/mockAPI/resultData.json')
+		.then((response) => {
+			dispatch(setResult(response.data.results));
+			dispatch(setAnalysis(response.data.analysis));
+			dispatch(setFeedback(response.data.feedback));
+		})
+		.catch((error) => {
+			console.log('Failed to fetch result data, ', error.message);
+		});
+}
+
+// 서버로부터 받은 결과 저장
+export function setResult(results) {
+	return {
+		type: 'RESULT_FETCH',
+		payload: results,
+	};
+}
+export function setAnalysis(analysis) {
+	return {
+		type: 'RESULT_FETCH_ANALYSIS',
+		payload: analysis,
+	};
+}
+export function setFeedback(feedback) {
+	return {
+		type: 'RESULT_FETCH_FEEDBACK',
+		payload: feedback,
+	};
+}
+
+export function setResultFocusNo(num) {
+	return {
+		type: 'RESULT_FOCUS_NO',
+		payload: num,
+	};
+}
+
+export function setResultFocusType(type) {
+	return {
+		type: 'RESULT_FOCUS_TYPE',
+		payload: type,
+	};
+}
---
+++ client/src/reducers/resultRedcuer.js
@@ -0,0 +1,78 @@
+const initialState = {
+	// 표시할 문제 번호
+	focusNo: 1,
+	// 표시할 결과 타입
+	focusType: 'readability',
+	// 결과 정보
+	results: [
+		{
+			// 문제 번호
+			problemNo: 1,
+			// 제출한 언어
+			language: 'c',
+			// 제출 코드
+			userCode: '',
+			// 가독성
+			readability: '',
+			// 시간 복잡도
+			timeComplexity: '',
+			// 개선 코드
+			improvedCode: '',
+		},
+		{
+			// 문제 번호
+			problemNo: 2,
+			// 제출한 언어
+			language: 'cpp',
+			// 제출 코드
+			userCode: '',
+			// 가독성
+			readability: '',
+			// 시간 복잡도
+			timeComplexity: '',
+			// 개선 코드
+			improvedCode: '',
+		},
+	],
+	// 분석
+	analysis: '',
+	// 피드백
+	feedback: '',
+};
+
+function resultReducer(state = initialState, action) {
+	switch (action.type) {
+		// 문제 번호 선택
+		case 'RESULT_FOCUS_NO':
+			return {
+				...state,
+				focusNo: action.payload,
+			};
+		// 화면에 표시할 결과 선택
+		case 'RESULT_FOCUS_TYPE':
+			return {
+				...state,
+				focusType: action.payload,
+			};
+		// 서버로부터 받은 결과 저장
+		case 'RESULT_FETCH':
+			return {
+				...state,
+				results: action.payload,
+			};
+		case 'RESULT_FETCH_ANALYSIS':
+			return {
+				...state,
+				analysis: action.payload,
+			};
+		case 'RESULT_FETCH_FEEDBACK':
+			return {
+				...state,
+				feedback: action.payload,
+			};
+		default:
+			return state;
+	}
+}
+
+export default resultReducer;
