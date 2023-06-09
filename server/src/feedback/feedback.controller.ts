1c38963f728551c5bc7581cb9dc9321d07482335
lincret 4shley <lentical.n17@gmail.com>
2023-06-09 17:53:39 +0900

    feat: (back) 피드백 및 인터뷰 관련 함수 구체화 작업
    front에서 받을 양식을 고려해서 코드 피드백 시에 피드백하는 항목에 관한 파라미터 추가. 인터뷰 질문에 대한 응답 평가 함수
    생성.

--- server/src/feedback/feedback.controller.ts
+++ server/src/feedback/feedback.controller.ts
@@ -1,22 +1,38 @@
 import { Body, Controller, Get } from '@nestjs/common';
 import { FeedbackService } from './feedback.service';
 import { CodingTestDto } from './dto/coding-test.dto';
+import { InterviewSetDto } from './dto/interview-set.dto';
 
 @Controller('feedback')
 export class FeedbackController {
 	constructor(private readonly feedbackService: FeedbackService) {}
 
-	@Get('coding-test/completion')
-	async codingTestCompletion(@Body() codingTestDto: CodingTestDto) {
-		for (let i = 0; codingTestDto.problem.length; i++) {
-			const str = this.feedbackService.getCodeEvaluation(codingTestDto.problem[i].code);
-			console.log(str);
-		}
+	// Experimental Code
+	@Get('coding-test/trial')
+	async codingTestCompletionTrial() {
+		const str = this.feedbackService.getCodeEvaluation('this is a code sent to GPT.');
+		return str;
 	}
 
-	@Get('coding-test/trial') // Experimental Code
-	async codingTestCompletionTrial() {
-		const str = this.feedbackService.getCodeEvaluation('this is a code.');
-		console.log(str);
+	@Get('coding-test/completion')
+	async codingTestCompletion(@Body() codingTestDto: CodingTestDto) {
+		const strarr = new Array(codingTestDto.problem.length);
+		for (let i = 0; codingTestDto.problem.length; i++) {
+			const str = this.feedbackService.getCodeEvaluation(codingTestDto.problem[i].code);
+			strarr.push(str);
+		}
+		return strarr;
+	}
+	@Get('coding-test/interview')
+	async interviewProblem(@Body() interviewSetDto: InterviewSetDto) {
+		const strarr = new Array(interviewSetDto.interviewData.length);
+		for (let i = 0; interviewSetDto.interviewData.length; i++) {
+			const str = this.feedbackService.getInterviewFeedback(
+				interviewSetDto.interviewData[i].description,
+				interviewSetDto.interviewData[i].userResponse
+			);
+			strarr.push(str);
+		}
+		return strarr;
 	}
 }
--- server/src/feedback/feedback.service.ts
+++ server/src/feedback/feedback.service.ts
@@ -4,7 +4,27 @@ import { OpenaiService } from './openai.service';
 @Injectable()
 export class FeedbackService {
 	constructor(private readonly openaiService: OpenaiService) {}
+
 	getCodeEvaluation(code: string) {
-		return this.openaiService.sendToChatGPT(code);
+		return this.getCodeEvaluationWithType(code, 'general');
+	}
+
+	getCodeEvaluationWithType(code: string, resultType: string) {
+		let str: string;
+		switch (resultType) {
+			case 'readability':
+				str = 'How is the readability of the follwing code?\n\n' + code;
+			case 'timeComplexity':
+				str = 'How is the time complexity of the follwing code?\n\n' + code;
+			case 'improvement':
+				str = 'How can the following code be improved?\n\n' + code;
+			case 'analysis':
+				str = 'Can you breifly give us an analysis of the following code?\n\n' + code;
+			case 'feedback':
+				str = 'Can you breifly give us a feedback of the following code?\n\n' + code;
+			default:
+				str = code;
+		}
+		return this.openaiService.sendToChatGPT(str);
 	}
 }
