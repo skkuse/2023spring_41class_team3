e132253528dac526c94b24431a999dc6a5da1e64
jiyu <wkdduq4840@gmail.com>
2023-06-10 21:58:05 +0900

    feat: (back) add api for get coding test problem set
    GET /api/problem/codingtest?difficulty=<number>&num=<number>

--- server/src/problem/problem.controller.ts
+++ server/src/problem/problem.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Get, Post, Body, Param, Query } from '@nestjs/common';
 import { CreateProblemDto } from './dto/create-problem.dto';
 import { ProblemRepository } from './repository/problem.repository';
+import { ProblemService } from './problem.service';
 
 @Controller('problem')
 export class ProblemController {
-	constructor(private readonly problemRepository: ProblemRepository) {}
+	constructor(
+		private readonly problemRepository: ProblemRepository,
+		private readonly problemService: ProblemService
+	) {}
 
 	@Post('')
 	create(@Body() createProblemDto: CreateProblemDto) {
@@ -16,13 +20,13 @@ export class ProblemController {
 		return this.problemRepository.getCodetestSet(+difficulty, +num);
 	}
 
+	@Get('codingtest')
+	getCodeSet(@Query('difficulty') difficulty: string, @Query('num') num: string) {
+		return this.problemService.getCodingtestSet(+difficulty, +num);
+	}
+
 	@Get(':id')
 	findOne(@Param('id') id: string) {
 		return this.problemRepository.findOne(id);
-	}
-
-	@Get('codingtest')
-	getCodeSet(@Query('difficulty') difficulty: string, @Query('num') num: string) {
-		return;
 	}
 }
