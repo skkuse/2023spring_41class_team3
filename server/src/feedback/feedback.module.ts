9bd07d1fc1fa3bbc17f3ab17fb1acb29fc5e8e69
lincret 4shley <49122276+lincret@users.noreply.github.com>
2023-06-11 04:57:00 +0900

    fix: (back) openai 관련 provider 문제와 import 문제 대응 (#40)
    * fix: (back) feedback.module 내 provider 추가 적용
    
    FeedbackService, OpenaiService를 provider로 추가 적용.
    
    * fix: (back) ChatGPTAPI 호출 방식 변경 (await 사용)
    
    import { ChatGPTAPI } from 'chatgpt'로는 정상적인 import가 되지 않는 것으로 확인되어,
    await import('chatgpt')를 사용하도록 메소드 내부 내용 조정했습니다.

--- server/src/feedback/feedback.module.ts
+++ server/src/feedback/feedback.module.ts
@@ -1,10 +1,12 @@
 import { ConfigService } from '@nestjs/config';
 import { FeedbackController } from './feedback.controller';
 import { Module } from '@nestjs/common';
+import { OpenaiService } from './openai.service';
+import { FeedbackService } from './feedback.service';
 
 @Module({
 	imports: [],
 	controllers: [FeedbackController],
-	providers: [ConfigService],
+	providers: [ConfigService, FeedbackService, OpenaiService],
 })
 export class FeedbackModule {}
--- server/src/feedback/openai.service.ts
+++ server/src/feedback/openai.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { ChatGPTAPI } from 'chatgpt';
+// import { ChatGPTAPI } from 'chatgpt';
 import { OPENAI_API_OPTIONS } from '../constant/openai.constant';
 
 @Injectable()
@@ -9,20 +9,23 @@ export class OpenaiService {
 
 	// Experimental Code
 	async sendExampleToChatGPT() {
-		const api = new ChatGPTAPI(OPENAI_API_OPTIONS);
+		const api = new (await import('chatgpt')).ChatGPTAPI(OPENAI_API_OPTIONS);
+		// const api = new ChatGPTAPI(OPENAI_API_OPTIONS); // Causing problem with importing ChatGPTAPI...
 		const res = await api.sendMessage('Hello World!');
 		return res.text;
 	}
 
 	async sendToChatGPT(message: string) {
-		const api = new ChatGPTAPI(OPENAI_API_OPTIONS);
+		const api = new (await import('chatgpt')).ChatGPTAPI(OPENAI_API_OPTIONS);
+		// const api = new ChatGPTAPI(OPENAI_API_OPTIONS); // Causing problem with importing ChatGPTAPI...
 		const res = await api.sendMessage(message);
 		return res.text;
 	}
 
 	async sendToChatGPTWithParentMessage(message: string, id: string) {
-		const api = new ChatGPTAPI(OPENAI_API_OPTIONS);
-		//  id comes from 'res.id', where 'res' is the result of 'await api.sendMessage(message)'
+		const api = new (await import('chatgpt')).ChatGPTAPI(OPENAI_API_OPTIONS);
+		// const api = new ChatGPTAPI(OPENAI_API_OPTIONS); // Causing problem with importing ChatGPTAPI...
+		// id comes from 'res.id', where 'res' is the result of 'await api.sendMessage(message)'
 		const res = await api.sendMessage(message, {
 			parentMessageId: id,
 		});
