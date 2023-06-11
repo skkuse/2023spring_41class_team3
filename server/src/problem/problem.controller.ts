342bad7dae82aa9158d2c5c74778fe92cc6bcddf
AnJeongMin <ajhappy12@gmail.com>
2023-06-11 19:57:01 +0900

    refactor: (back) 사용하지 않는 api 제거

--- server/src/problem/problem.controller.ts
+++ server/src/problem/problem.controller.ts
@@ -1,23 +1,14 @@
-import { Controller, Get, Post, Body, Param, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { CreateProblemDto } from './dto/create-problem.dto';
 import { ProblemRepository } from './repository/problem.repository';
-import { ProblemService } from './problem.service';
 
 @Controller('problem')
 export class ProblemController {
-	constructor(
-		private readonly problemRepository: ProblemRepository,
-		private readonly problemService: ProblemService
-	) {}
+	constructor(private readonly problemRepository: ProblemRepository) {}
 
 	@Post('')
 	create(@Body() createProblemDto: CreateProblemDto) {
 		return this.problemRepository.createProblem(createProblemDto);
-	}
-
-	@Get('')
-	getCodeSetIDs(@Query('difficulty') difficulty: string, @Query('num') num: string) {
-		return this.problemRepository.getRandomProblemIdList(+difficulty, +num);
 	}
 
 	@Get(':id')
