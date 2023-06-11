f41773c2c5cfb7902f46fdde8d3103f8c6f7a9dc
AnJeongMin <ajhappy12@gmail.com>
2023-06-11 20:56:54 +0900

    feat: (back) 코딩 테스트 문제 리스트 api 구현

--- server/src/coding-test/controller/coding-test.controller.ts
+++ server/src/coding-test/controller/coding-test.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { CodingTestService } from '../service/coding-test.service';
+import { ProblemService } from 'src/problem/problem.service';
+import { TestProblemListDto } from '../dto/testProblemList.dto';
 
 @Controller('coding-test')
 export class CodingTestController {
-	constructor(private readonly codingTestService: CodingTestService) {}
+	constructor(
+		private readonly codingTestService: CodingTestService,
+		private readonly problemService: ProblemService
+	) {}
+
+	@Get('problems')
+	async getTestProblemList(@Query() testProblemListDto: TestProblemListDto) {
+		return await this.problemService.getTestProblemList(testProblemListDto);
+	}
 }
