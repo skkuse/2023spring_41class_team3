dba7d9a1dc5c6cc703389dfd0f503c36e26a4ff0
Suhyun Han <01tngus@naver.com>
2023-06-11 14:03:48 +0900

    fix: (front) 다른 문제로 이동 시 남은시간 초기화 문제 해결

--- client/src/components/ps-test/ContestTimer.jsx
+++ client/src/components/ps-test/ContestTimer.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
-import Countdown from 'react-countdown';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
+const formatTime = (milliseconds) => {
+	const seconds = Math.floor(milliseconds / 1000);
+	const minutes = Math.floor(seconds / 60);
+
+	const formattedMinutes = String(minutes).padStart(2, '0');
+	const formattedSeconds = String(seconds % 60).padStart(2, '0');
+
+	return `${formattedMinutes}:${formattedSeconds}`;
+};
+
 function ContestTimer() {
+	const [executionTime, setExecutionTime] = useState(0);
+
+	useEffect(() => {
+		const startTime = performance.now();
+
+		const interval = setInterval(() => {
+			const currentTime = performance.now();
+			const time = currentTime - startTime;
+
+			setExecutionTime(time);
+		}, 1000);
+
+		return () => {
+			clearInterval(interval);
+		};
+	}, []);
+
 	const { timeLimit } = useSelector((state) => state.contestSetting);
 
-	return <Countdown date={Date.now() + timeLimit} daysInHours />;
+	const formattedTime = formatTime(timeLimit - executionTime);
+
+	return <div> {formattedTime}</div>;
 }
 
 export default ContestTimer;
