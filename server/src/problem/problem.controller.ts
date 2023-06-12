31d098cf6a2a105aed7bb2746c6b48df9d735e90
AnJeongMin <ajhappy12@gmail.com>
2023-06-12 15:27:12 +0900

    feat: (back) code test api end-point 구현

--- server/src/problem/problem.controller.ts
+++ server/src/problem/problem.controller.ts
@@ -1,10 +1,15 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { CreateProblemDto } from './dto/create-problem.dto';
 import { ProblemRepository } from './repository/problem.repository';
+import { ProblemTestDto } from './dto/problemTest.dto';
+import { ProblemService } from './problem.service';
 
 @Controller('problem')
 export class ProblemController {
-	constructor(private readonly problemRepository: ProblemRepository) {}
+	constructor(
+		private readonly problemRepository: ProblemRepository,
+		private readonly problemService: ProblemService
+	) {}
 
 	@Post('')
 	create(@Body() createProblemDto: CreateProblemDto) {
@@ -14,5 +19,10 @@ export class ProblemController {
 	@Get(':id')
 	findOne(@Param('id') id: string) {
 		return this.problemRepository.findOne(id);
+	}
+
+	@Post('test')
+	problemTest(@Body() problemTestDto: ProblemTestDto) {
+		return this.problemService.codeSubmit(problemTestDto);
 	}
 }
