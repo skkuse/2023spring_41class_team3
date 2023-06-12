c3bc5c77eac49cdfd3810240c3fbd0ec6b2ced0b
AnJeongMin <ajhappy12@gmail.com>
2023-06-12 15:28:10 +0900

    feat: (back) 코드 채점 로직 구현
    임시로 python code 채점만 구현

--- server/src/constant/coding-test.constant.ts
+++ server/src/constant/coding-test.constant.ts
@@ -1,1 +1,2 @@
 export const PS_UNIT_TIME = 10 * 60 * 1000; // 10 minutes
+export const PS_TIME_OUT = 1000; // 1 second
--- server/src/problem/problem.service.ts
+++ server/src/problem/problem.service.ts
@@ -2,6 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { ProblemRepository } from './repository/problem.repository';
 import { TestInitDto } from 'src/coding-test/dto/testInit.dto';
 import { Problem } from '@type';
+import { exec } from 'child_process';
+import fs from 'fs';
+import { testcase } from './dto/create-problem.dto';
+import { ProblemTestDto } from './dto/problemTest.dto';
+import { PS_TIME_OUT } from '@constant';
+import { v4 } from 'uuid';
+import { CaseResultDto } from './dto/caseResult.dto';
+import { TestResultDto } from './dto/testResult.dto';
 
 @Injectable()
 export class ProblemService {
@@ -21,5 +29,75 @@ export class ProblemService {
 			outputDescription: problemData.outputDescription,
 			testcases: problemData.testcases,
 		}));
+	}
+
+	async codeSubmit({ code, language, testcases }: ProblemTestDto) {
+		const runId = v4();
+		switch (language) {
+			case 'python':
+				const fileName = `${runId}.py`;
+				const command = `python3 ${fileName}`;
+				return this.runTest({ code, testcases, fileName, command });
+			default:
+				return;
+		}
+	}
+
+	async runTest({ code, testcases, fileName, command }) {
+		fs.writeFileSync(`./${fileName}`, code);
+
+		const testList = testcases.map((testcase: testcase, testIdx: number) => {
+			const { input, output } = testcase;
+			return new Promise((resolve, reject) => {
+				const start = Date.now();
+				exec(
+					`echo ${input} | ${command}`,
+					{ timeout: PS_TIME_OUT },
+					(error, stdout, stderr) => {
+						const end = Date.now();
+						const time = end - start;
+						let isSuccess: boolean, isError: boolean, message: string;
+
+						if (error || stderr) {
+							isSuccess = false;
+							isError = true;
+							if (error.signal === 'SIGTERM') {
+								message = `time out: ${PS_TIME_OUT}ms`;
+							} else {
+								reject(stderr);
+							}
+						} else {
+							isError = false;
+							if (stdout != output) {
+								message = `mismatch output: ${stdout.trim()}`;
+								isSuccess = false;
+							}
+							isSuccess = true;
+						}
+						const testResult = new CaseResultDto(
+							stdout.trim(),
+							testIdx,
+							isSuccess,
+							isError,
+							time,
+							message
+						);
+
+						resolve(testResult);
+					}
+				);
+			});
+		});
+
+		try {
+			const caseResultList = await Promise.all(testList);
+			return new TestResultDto({ code, run: true, caseResultList });
+		} catch (err) {
+			return new TestResultDto({ code, run: false, message: err });
+		} finally {
+			fs.unlink(`./${fileName}`, (err) => {
+				if (err) throw err;
+			});
+		}
 	}
 }
