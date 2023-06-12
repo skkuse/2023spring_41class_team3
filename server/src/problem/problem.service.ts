18fef588ca3ac2f413351e48b0c65fa188eeace2
AnJeongMin <ajhappy12@gmail.com>
2023-06-13 04:33:07 +0900

    refactor: (back) test case 로직 리팩토링

--- server/src/problem/problem.service.ts
+++ server/src/problem/problem.service.ts
@@ -36,57 +36,22 @@ export class ProblemService {
 
 	async codeSubmit({ code, language, testcases }: ProblemTestDto) {
 		const runId = v4();
+		let fileName: string, command: string;
 		switch (language) {
 			case 'python':
-				const fileName = `${runId}.py`;
-				const command = `python3 ${fileName}`;
-				return this.runTest({ code, testcases, fileName, command });
+				fileName = `${runId}.py`;
+				command = `python3`;
+				return this.runScriptTest({ code, testcases, fileName, command });
 			default:
 				return;
 		}
 	}
 
-	async runTest({ code, testcases, fileName, command }) {
+	async runScriptTest({ code, testcases, fileName, command }) {
 		fs.writeFileSync(`./${fileName}`, code);
 
 		const testList = testcases.map((testcase: testcase, testIdx: number) => {
-			const { input, output } = testcase;
-			return new Promise((resolve, reject) => {
-				const start = Date.now();
-				exec(
-					`echo ${input} | ${command}`,
-					{ timeout: PS_TIME_OUT },
-					(error, stdout, stderr) => {
-						const end = Date.now();
-						const time = end - start;
-						let isSuccess: boolean, message: string;
-
-						if (error || stderr) {
-							isSuccess = false;
-							if (error.signal === 'SIGTERM') {
-								message = `time out: ${PS_TIME_OUT}ms`;
-							} else {
-								reject(stderr);
-							}
-						} else if (stdout != output) {
-							message = `mismatch output: ${stdout.trim()}`;
-							isSuccess = false;
-						} else {
-							isSuccess = true;
-						}
-
-						const testResult = new CaseResultDto(
-							stdout.trim(),
-							testIdx,
-							isSuccess,
-							time,
-							message
-						);
-
-						resolve(testResult);
-					}
-				);
-			});
+			return this.runTestCase({ testIdx, testcase, fileName, command });
 		});
 
 		try {
@@ -99,5 +64,45 @@ export class ProblemService {
 				if (err) throw err;
 			});
 		}
+	}
+
+	runTestCase({ testIdx, testcase, fileName, command }): Promise<CaseResultDto> {
+		const { input, output } = testcase;
+		return new Promise((resolve, reject) => {
+			const start = Date.now();
+			exec(
+				`echo ${input} | ${command} ${fileName}`,
+				{ timeout: PS_TIME_OUT },
+				(error, stdout, stderr) => {
+					const end = Date.now();
+					const time = end - start;
+					let isSuccess: boolean, message: string;
+
+					if (error || stderr) {
+						isSuccess = false;
+						if (error.signal === 'SIGTERM') {
+							message = `time out: ${PS_TIME_OUT}ms`;
+						} else {
+							reject(stderr);
+						}
+					} else if (stdout != output) {
+						message = `mismatch output: ${stdout.trim()}`;
+						isSuccess = false;
+					} else {
+						isSuccess = true;
+					}
+
+					const testResult = new CaseResultDto(
+						stdout.trim(),
+						testIdx,
+						isSuccess,
+						time,
+						message
+					);
+
+					resolve(testResult);
+				}
+			);
+		});
 	}
 }
