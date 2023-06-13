c9c1ef2dbc3baa48914c3c613048dfdc807e0af8
AnJeongMin <ajhappy12@gmail.com>
2023-06-13 11:27:22 +0900

    fix: (back) 이스케이프 string 변환 메서드 수정

--- client/src/utils/problem.js
+++ client/src/utils/problem.js
@@ -1,5 +1,5 @@
 function convertToEscapedString(code) {
-	return code.replace(/\n/g, '\\n').replace(/\t/g, '\\t');
+	return code.replace(/\n/g, '\n').replace(/\t/g, '\t').replace(/"/g, '"');
 }
 
 export default convertToEscapedString;
